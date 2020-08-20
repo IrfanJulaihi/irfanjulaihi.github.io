928b43a9055ab6950353b0f69a326d869dd27eb7
IrfanJulaihi <48781387+IrfanJulaihi@users.noreply.github.com>
2020-08-20 09:42:13 +0800

    Change type animation to Front-end developer(Vue/React)

--- js/typeAnimation.js
+++ js/typeAnimation.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])    
- consoleText(['IoT Software Engineer'], 'text',['#3f3']);
+ consoleText(['Front End Develepor(Vue/React)'], 'text',['#3f3']);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
